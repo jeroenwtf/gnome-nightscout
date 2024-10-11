86a3166843638bf7f0d7fb85be327d854d7e9972
Jeroen van Meerendonk <hola@jeroen.wtf>
2024-10-11 14:44:12 +0200

    Fix bug in the link script

--- link.sh
+++ link.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-folder_in_repository=$HOME/Code/gnome-nightscout/nightscout@jeroen.wtf
-folder_in_extensions=$HOME/.local/share/gnome-shell/extensions/nightscout@jeroen.wtf
+folder_in_repository="$HOME/Code/gnome-nightscout/nightscout@jeroen.wtf"
+folder_in_extensions="$HOME/.local/share/gnome-shell/extensions/nightscout@jeroen.wtf"
 
-cd $folder_in_repository
+cd "$folder_in_repository" || exit
 
 glib-compile-schemas schemas/
 
-if [ ! -L "$folder_in_extensions" ]; then
+if [ -L "$folder_in_extensions" ]; then
+  # It's a symlink, remove it
+  rm "$folder_in_extensions"
+elif [ -d "$folder_in_extensions" ]; then
+  # It's a directory, remove it
   rm -rf "$folder_in_extensions"
 fi
 
-ln -s $folder_in_repository $folder_in_extensions
+ln -sf "$folder_in_repository" "$folder_in_extensions"
 
